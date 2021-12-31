8f6fc53f91a4cd65ff6d5b4edda3e7ef289dae58
Carlos Eduardo Castro do Carmo <carloseduardocastrodocarmo@gmail.com>
2021-12-31 13:06:12 -0300

    Projeto finalizado, subindo para o GIT

---
+++ CourseCarRental/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseCarRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
